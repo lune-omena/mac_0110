1323a1d8fad7d5d6c959fb559203eab2b619129b
Lucy Anne de Omena Evangelista <lucy.omena@live.com>
2019-05-24 18:26:46 -0300

    Ajustando o EP3, e adicionando uma versao comentada. Features inclusas: 5/6.

--- EP3/ep3_comentado.cpp
+++ EP3/ep3_comentado.cpp
@@ -35,16 +35,18 @@ int main(){
 	printf("Benvindo ao Sistema Estatistico de Uso do Salao de Estudos");
 	printf("\nBiblioteca \"Carlos Benjamim de Lyra\"  -  IME-USP");
 	printf("\n\nDigite o nome do arquivo de dados: ");
-	scanf("%s", nome_arquivo);
-	arquivo = fopen(nome_arquivo, "r");
+	//scanf("%s", nome_arquivo);
+	arquivo = fopen("arq20190425.txt", "r");
 
 	// Armazenando os dados do arquivo de dados, numa estrutura de sequencia [nusp, entrada/saida, tempo(min)]
 	while (!feof(arquivo)){		
-		if (fscanf(arquivo,"%d %d %d:%d", &sis[1][tamn], &sis[0][tamn], &hrs , &min) != 4)
+		if (fscanf(arquivo,"%d %d %d:%d", &sis[1][tamn], &sis[0][tamn], &hrs , &min) != 4){
 			continue;
-		
+		}
+			
 		tempo = hrs*60 + min;
 		sis[2][tamn] = tempo;
+		//printf("\n%d %d %d", sis[0][tamn], sis[1][tamn], sis[2][tamn]);
 		tamn++;
 	}
 	
@@ -63,6 +65,7 @@ int main(){
 			registro[1][k] = sis[2][i]; //minutos de entrada
 			registro[2][k] = sis[2][achouvalor] - sis[2][i]; //permanencia
 			sis[0][achouvalor] = 0; //zerando o nusp, para não considerar
+			//printf("\n %d \t %d \t %d \t %d", k, registro[0][k], registro[1][k], registro[2][k]);
 			k++;
 		} else {
 			// se só identificou a entrada da pessoa considera ela saindo as 23:59
@@ -70,6 +73,7 @@ int main(){
 				registro[0][k] = sis[0][i]; //nusp
 				registro[1][k] = sis[2][i]; //minutos de entrada
 				registro[2][k] = 1439 - sis[2][i]; //permanencia, descontando a entrada do total de minutos de um dia
+			//	printf("\n %d \t %d \t %d \t %d", k, registro[0][k], registro[1][k], registro[2][k]);
 				k++;
 			} else {
 				// se só identificou a saída da pessoa, considera como ela entrando a meia noite (00:00)
@@ -77,12 +81,14 @@ int main(){
 				registro[0][k] = sis[0][i]; //nusp
 				registro[1][k] = 0; //minutos de entrada
 				registro[2][k] = sis[2][i]; //permanencia
+			//	printf("\n %d \t %d \t %d \t %d", k, registro[0][k], registro[1][k], registro[2][k]);
 				k++;
 				}
 			}
 		}	
 	}
 	// Agora a variável k representa a quantidade de visitas diárias à biblioteca!
+	
 	
 	// Calculando status
 	printf("\n\nNO PERIODO TIVEMOS %3d USUARIOS DIFERENTES E %3d VISITAS", nusp_unicos(registro, k), k);
@@ -117,14 +123,16 @@ int nusp_unicos(int nusp[3][100], int tamn){
 			if (nusp[0][i] == unicos[j])
 				possui++;
 		}
-		
+
 		if (possui == 0){
+		//	printf("\nnusp: %d tam: %d", nusp[0][i], tamun);
 			unicos[tamun] = nusp[0][i];
 			tamun++;
 		}
 		possui = 0;
 	}
 	return(tamun);
+
 }
 
 float tempo_medio(int dados[3][100], int tamn, int col){
@@ -139,11 +147,12 @@ float tempo_medio(int dados[3][100], int tamn, int col){
 float desvio_padrao(int dados[3][100], int tamn, int col){
 	int i;
 	float desvio, media, soma = 0;
-	
 	media = tempo_medio(dados, tamn, col);
+	
 	for (i = 0; i < tamn; i++){
 		soma += pow(dados[2][i] - media, 2); 
 	}
+	
 	desvio = sqrt(soma/tamn);
 	
 	return (desvio);
@@ -153,15 +162,18 @@ void histograma_de_uso(int dados[3][100], int tamn){
 	int i, j, nusp = 0, hora[24], inicio, fim;
 	
 	//zerando a matriz de horas
-	for(i = 0; i < 24; i++)
+	for(i = 0; i < 24; i++){
 		hora[i] = 0;
+	}
 	
 	// Para cada registro, contabiliza as horas em que o estudante esteve na biblioteca
 	for (i = 0; i < tamn; i++){
 		inicio = dados[1][i] / 60;
 		fim = (dados[1][i] + dados[2][i])/ 60;
+		//printf("\ninicio: %d fim: %d\t%d\t%d", inicio, fim, dados[1][i], dados[2][i]);
 		while(inicio <= fim){
 		 	hora[inicio]++;
+		 //	printf ("\n hora: %d inicio: %d fim: %d", hora[inicio], inicio, fim);
 		 	inicio++;
 	 	}	
 	}
@@ -211,6 +223,7 @@ void ordena(int dados[3][100], int tamn, int col, int ordena[], int ascdesc){
         for(j = 0; j < tamn; j++){
         	// Para garantir que nao havera problemas com multiplas entradas de estudantes, ha a condicao ordena[j] == -1 e um break
             if(vaux[i] == dado[j] && ordena[j] == -1){
+            	// printf("\n %d %d %d %d %d", vaux[i], dados[col][j], i, j, ordena[j]);
             	ordena[j] = i;
             	break; // barrando a continuação das comparações
             }
